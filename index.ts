db6ed207451f39392aff53202b7c35ea96deacb4
Abiskar Lamichhane <abiskar.lamichhane@swifttech.com>
2025-01-28 11:35:16 +0545

    Added dummyfile and tsc setup

--- index.ts
+++ index.ts
@@ -1,60 +1,75 @@
-import { Email, User, emails, CarModel, EmailClone, Str } from "./types";
+// import { Email, User, CarModel, EmailClone, Str } from "./types";
+// import emails from "./data/dummyemails.json";
 
-const GetData = <Type extends User, key extends keyof Type>(
-  user: Type,
-  property: key
-) => {
-  return user[property];
-};
+import { AddEmailData, DeleteEmail } from "./actions/email-actions";
 
-// console.log(
-//   GetData(
-//     {
-//       name: "Abiskar",
-//       email: "abinjr08@gmail.com",
-//       age: 31,
-//       password: "abiskar",
-//     },
-//     "password"
-//   )
-// );
+// const GetData = <Type extends User, key extends keyof Type>(
+//   user: Type,
+//   property: key
+// ) => {
+//   return user[property];
+// };
 
-const findEmailData = (id: string): Email => {
-  const findEmail = emails.find((email) => email.emailId === id);
+// // console.log(
+// //   GetData(
+// //     {
+// //       name: "Abiskar",
+// //       email: "abinjr08@gmail.com",
+// //       age: 31,
+// //       password: "abiskar",
+// //     },
+// //     "password"
+// //   )
+// // );
 
-  if (!findEmail) {
-    throw new Error(`Email with ID ${id} not found`);
-  }
+// const findEmailData = (id: string): Email => {
+//   const findEmail = emails.find((email) => email.emailId === id);
 
-  return findEmail;
-};
+//   if (!findEmail) {
+//     throw new Error(`Email with ID ${id} not found`);
+//   }
 
-console.log(findEmailData("2"));
+//   return findEmail;
+// };
 
-const CarObject: CarModel = {
-  id: 101,
-  name: "Ferrari",
-  getName(id) {
-    return  this.name;
-  },
-  setName(name) {
-      this.name = name;
-  },
-};
+// // console.log(findEmailData("2"));
 
-CarObject.setName("Ferrrrariiiii")
-console.log(CarObject.getName(101))
+// const CarObject: CarModel = {
+//   id: 101,
+//   name: "Ferrari",
+//   getName(id) {
+//     return this.name;
+//   },
+//   setName(name) {
+//     this.name = name;
+//   },
+// };
 
+// // CarObject.setName("Ferrrrariiiii");
+// // console.log(CarObject.getName(101));
 
-const email1: EmailClone = {
-    emailId: "",
-    threadId: "",
-    subject: "",
-    from: "",
-    to: [],
-    cc: [],
-    bcc: [],
-    body: "",
-    sentDate: ""
-}
+// const email1: EmailClone = {
+//   emailId: "",
+//   threadId: "",
+//   subject: "",
+//   from: "",
+//   to: [],
+//   cc: [],
+//   bcc: [],
+//   body: "",
+//   sentDate: "",
+// };
 
+// AddEmailData({
+//   emailId: "21",
+//   threadId: "121",
+//   subject: "Meeting Reminder again",
+//   from: "john.doe@example.com",
+//   to: ["alice.smith@example.com", "bob.johnson@example.com"],
+//   cc: ["carol.white@example.com"],
+//   bcc: [],
+//   body: "This is a reminder for the meeting scheduled tomorrow at 10 AM.",
+//   sentDate: "2025-01-20T09:00:00Z",
+// });
+
+DeleteEmail("21")
